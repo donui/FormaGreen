809550fa2b9992757ed5c9c2574c9999e9dac043
ryan <karl.meudje@sinequanone-conseil.fr>
2020-08-02 15:13:20 +0200

    create application and domain

---
+++ Application/src/main/java/cm/supinfo/formagreen/application/Application.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.supinfo.formagreen.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author ryank
+ */
+
+@SpringBootApplication
+@ComponentScan(basePackages = "cm.supinfo.formagreen")
+@EntityScan(basePackages = {"cm.supinfo.formagreen.domain"})
+@EnableJpaRepositories(basePackages = {"cm.supinfo.formagreen.repository"})
+@PropertySource("classpath:application.properties")
+@EnableScheduling 
+public class Application extends SpringBootServletInitializer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
