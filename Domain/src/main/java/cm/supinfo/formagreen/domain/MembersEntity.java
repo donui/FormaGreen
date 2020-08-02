a4a4b8ff3bccb0a55d70af68ea751e51a446d91c
ryan <karl.meudje@sinequanone-conseil.fr>
2020-08-02 16:00:46 +0200

    create domain entity

---
+++ Domain/src/main/java/cm/supinfo/formagreen/domain/MembersEntity.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.supinfo.formagreen.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.apache.commons.lang.Validate;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+
+/**
+ *
+ * @author ryank
+ */
+@Entity(name = "MembersEntity")
+@Table(name = "members")
+public class MembersEntity implements Serializable, Persistable<Long> {
+
+    @Id
+    @Column(nullable = false, name = "id")
+    @GeneratedValue(generator = "ALERT_SEQ_GEN")
+    @GenericGenerator(
+            name = "ALERT_SEQ_GEN",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                @Parameter(name = "sequence_name", value = "ALERT_SEQ_GEN"),
+                @Parameter(name = "initial_value", value = "1"),
+                @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private Long Id;
+
+    @Column(name = "name", nullable = false, unique = false)
+    private String name;
+
+    @Column(name = "firstname", nullable = false, unique = false)
+    private String firstname;
+
+    @Column(name = "age", nullable = false, unique = false)
+    private String age;
+
+    @Column(name = "sex", nullable = false, unique = false)
+    private Boolean sex;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "qr_code", nullable = false, unique = true)
+    private String qr_code;
+
+    @Column(name = "telephone", nullable = false, unique = false)
+    private String telephone;
+
+    @Column(name = "matricule", nullable = false, unique = false)
+    private String matricule;
+
+    @Column(name = "subscription_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.DATE)
+    private Date subscription_date;
+
+    @Column(name = "creation_date", nullable = false, updatable = false)
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationDate;
+    
+     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_typeId", nullable = false)
+    private Account_typeEntity account_typeId;
+
+
+    @Override
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getQr_code() {
+        return qr_code;
+    }
+
+    public void setQr_code(String qr_code) {
+        this.qr_code = qr_code;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public Date getSubscription_date() {
+        return subscription_date;
+    }
+
+    public void setSubscription_date(Date subscription_date) {
+        this.subscription_date = subscription_date;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean isNew() {
+        return Id == null;
+    }
+
+    @PrePersist
+    @PreUpdate
+    protected void prePersistAndPreUpdate() {
+        Validate.notNull(creationDate, "createDate must not be null");
+        Validate.isTrue(creationDate.getTime() < System.currentTimeMillis(), "creationDate cannot be in the past");
+    }
+
+}
